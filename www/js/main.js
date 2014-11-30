907643d7ef85d6080052526ef8e22527629393a2
Santiago Lizardo <santiago.lizardo@gmail.com>
2014-11-30 15:19:45 +0000

    Small tests

--- www/js/main.js
+++ www/js/main.js
@@ -11,10 +11,11 @@ require.config({
         	'phaser': {
         		exports: 'Phaser'
         	}
-        }
+        },
+	urlArgs: 'bust=' + Date.now()
 });
 
-require( [ 'phaser' ], function( Phaser ) {
+require( [ 'phaser', 'jquery' ], function( Phaser, $ ) {
 	var game = new Phaser.Game( 400, 490, Phaser.AUTO, 'gameDiv' );
 
 	var mainState = {
@@ -22,41 +23,67 @@ require( [ 'phaser' ], function( Phaser ) {
 		preload: function() { 
 			game.stage.backgroundColor = '#6ED2E5';
 			game.load.image( 'fish', 'img/fish.png' ); 
+			game.load.image( 'flower', 'img/flower.png' ); 
 		},
 
 		create: function() { 
 			game.physics.startSystem( Phaser.Physics.ARCADE );
-			game.physics.startSystem( Phaser.Physics.P2JS );
-			game.physics.p2.defaultRestitution = 0.8;
 
-			this.fish = this.game.add.sprite(100, 245, 'fish' );
-			this.fish.anchor.set( .5 );
-
-			game.physics.p2.enable( this.fish );
-			this.fish.body.setZeroDamping();
-			this.fish.body.fixedRotation = true;
+			this.createPlayer();
 
 			this.cursors = game.input.keyboard.createCursorKeys();
+
+			setInterval( $.proxy( this.addRandomFlower, this ), 1200 );
 		},
 
+		createPlayer: function() {
+			this.fish = this.game.add.sprite(100, 245, 'fish' );
+			game.physics.arcade.enable( this.fish );
+			this.fish.anchor.set( 20, 44 );
+			this.fish.body.collideWorldBounds=true;
+			this.fish.body.fixedRotation = true;
+			//this.fish.body.onBeginContact.add(this.blockHit, this);
+		},
+
+		addRandomFlower: function() {
+			var rnd = new Phaser.RandomDataGenerator();
+
+			var x = rnd.integerInRange( 20, 400 ), y = 0;
+			this.flower = flower = this.game.add.sprite( x, y, 'flower' );
+			game.physics.arcade.enable( this.flower );
+
+			// Add velocity to the flower to make it move left
+			flower.body.velocity.x = rnd.integerInRange(-10, 10); 
+			flower.body.velocity.y = rnd.integerInRange(20, 70); 
+
+			// Kill the flower when it's no longer visible 
+			flower.checkWorldBounds = true;
+			flower.outOfBoundsKill = true;
+		},
+	
 		update: function() {
-			this.fish.body.setZeroVelocity();
 			if (this.cursors.left.isDown) {
-				this.fish.body.moveLeft(400);
+				this.fish.body.x -= 10;
 			}
 			else if (this.cursors.right.isDown) {
-				this.fish.body.moveRight(400);
+				this.fish.body.x += 5;
 			}
 			if (this.cursors.up.isDown) {
-				this.fish.body.moveUp(100);
+				this.fish.body.y -= 5;
 			}
 			else if (this.cursors.down.isDown) {
-				this.fish.body.moveDown(200);
+				this.fish.body.y += 10;
 			}
+
+			game.physics.arcade.collide( this.fish, this.flower, this.blockHit, null, this );
 		},
+
+		blockHit: function( fish, flower ) {
+			flower.kill();
+		}
 	};
 
-	game.state.add('main', mainState);  
-	game.state.start('main');
+	game.state.add( 'main', mainState );  
+	game.state.start( 'main' );
 } );
 
