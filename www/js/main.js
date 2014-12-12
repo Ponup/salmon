908eb904531fcc00f260a4f0561bc8ca4f013e93
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2014-12-12 12:42:45 -0300

    primer update de los estilos y el nuevo layout del menu

--- www/js/main.js
+++ www/js/main.js
@@ -15,6 +15,20 @@ require.config({
 	urlArgs: 'bust=' + Date.now()
 });
 
+
+// modernizr code to check if browser support svg 
+if (!Modernizr.svg) {
+    var imgs = document.getElementsByTagName('img');
+    var svgExtension = /.*\.svg$/
+    var l = imgs.length;
+    for(var i = 0; i < l; i++) {
+        if(imgs[i].src.match(svgExtension)) {
+            imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
+            console.log(imgs[i].src);
+        }
+    }
+}
+
 define( function( require ) {
 	var context = require( 'data/context' ),
 		ScenesManager = require( 'pew/scenes/manager' ),
@@ -29,4 +43,3 @@ define( function( require ) {
 	context.scenesManager.add( new GameScene() );
 	context.scenesManager.switchTo( introScene );
 });
-
