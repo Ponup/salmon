d7cfc7ec652d9e19c9087a58d3a56d3872333458
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2014-12-19 12:39:47 -0300

    ajustado el tamaño del canvas, removido el movimiento vertical , centrado del personaje y otras mejoras en el css global

--- www/js/sprites/fish.js
+++ www/js/sprites/fish.js
@@ -11,8 +11,8 @@ define( function( require ) {
 		this.animations.add( 'left', [ 3, 2, 1, 0,1,2,3 ], 8,  false );
 		this.animations.add( 'idle', [ 9,10,11,12 ], 8,  true );
 		this.animations.add( 'right',[ 5, 6 ,7 , 8 ,7,6,5], 8,  false );
-		this.anchor.set( 20, 44 );
-		this.reset( game.world.centerX, game.world.centerY );
+		this.anchor.set( .5, .5 );
+		this.reset( game.world.centerX, game.world.height - 100 );
 		this.body.collideWorldBounds = true;
 		this.body.fixedRotation = true;
 	}
@@ -39,10 +39,12 @@ define( function( require ) {
 		}
 
 		if (cursors.up.isDown) {
-			this.body.y -= 5;
+			// remove temporally the up and down movement
+			// this.body.y -= 5;
 		}
 		else if (cursors.down.isDown) {
-			this.body.y += 10;
+			// remove temporally the up and down movement
+			// this.body.y += 10;
 		}
 	};
 
